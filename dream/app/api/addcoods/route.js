675abb76e2d1673aa3e36388d1d55dec357bde6c
ChefOmarDee <chefomardee@gmail.com>
2024-10-06 09:43:32 -0400

    added stuff

--- dream/app/api/addcoods/route.js
+++ dream/app/api/addcoods/route.js
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
-
+import { UpdateZipcodeCoordinates } from "@/app/_lib/mongodb/util/addzipcoords";
 
 export async function GET(req) {
     try {
         // Fetch counties from CSV
        
-
+        await UpdateZipcodeCoordinates();
         // Return the combined data as JSON
         return NextResponse.json({ hi:"hi" });
     } catch (error) {
