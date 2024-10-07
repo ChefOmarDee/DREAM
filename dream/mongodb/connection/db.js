a260b2c58590ae28275b5d155ea54927c01d130b
Aaron Tran <dahpaladin123@gmail.com>
2024-10-07 17:00:16 -0400

    m

--- dream/mongodb/connection/db.js
+++ dream/mongodb/connection/db.js
@@ -4,7 +4,7 @@ require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
 async function ConnectToDatabase() {
 	try {
-		await mongoose.connect("mongodb+srv://DreamHacks:Quoc@cluster0.rez7l.mongodb.net/DreamDB?retryWrites=true&w=majority&appName=Cluster0");
+		await mongoose.connect(process.env.MONGODB_URI);
 		console.log("Connected to MongoDB with Mongoose");
 	} catch (error) {
 		console.error("Error connecting to MongoDB:", error);
